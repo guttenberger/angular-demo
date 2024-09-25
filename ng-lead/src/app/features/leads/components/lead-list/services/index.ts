61eefaa17bef8e432925e2c2ee3edf53c7458cbb
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-09-25 16:12:16 +0000

    feat(leads): add lead routes

---
+++ ng-lead/src/app/features/leads/components/lead-list/services/index.ts
@@ -0,0 +1,5 @@
+export { LeadListService } from './lead-list.service';
+export { NewLeadListService } from './new-lead-list.service';
+export { PlacedLeadListService } from './placed-lead-list.service';
+export { QualifiedListService } from './qualified-lead-list.service';
+export { RejectedLeadListService } from './rejected-lead-list.service';
--- ng-lead/src/app/features/leads/leads-routing.module.ts
+++ ng-lead/src/app/features/leads/leads-routing.module.ts
@@ -4,12 +4,38 @@ import { RouterModule, Routes } from '@angular/router';
 import { LeadDetailComponent } from './components/lead-detail/lead-detail.component';
 import { LeadFormComponent } from './components/lead-form/lead-form.component';
 import { LeadListComponent } from './components/lead-list/lead-list.component';
+import {
+  LeadListService,
+  NewLeadListService,
+  PlacedLeadListService,
+  QualifiedListService,
+  RejectedLeadListService,
+} from './components/lead-list/services';
 import { LeadResolver } from './lead.resolver';
+import { LeadsStoreModule } from './store/leads-store.module';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'new',
     component: LeadListComponent,
+    providers: [{ provide: LeadListService, useClass: NewLeadListService }],
+  },
+  {
+    path: 'qualified',
+    component: LeadListComponent,
+    providers: [{ provide: LeadListService, useClass: QualifiedListService }],
+  },
+  {
+    path: 'placed',
+    component: LeadListComponent,
+    providers: [{ provide: LeadListService, useClass: PlacedLeadListService }],
+  },
+  {
+    path: 'rejected',
+    component: LeadListComponent,
+    providers: [
+      { provide: LeadListService, useClass: RejectedLeadListService },
+    ],
   },
   {
     path: 'create',
@@ -28,7 +54,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), LeadsStoreModule],
   exports: [RouterModule],
 })
 export class LeadsRoutingModule {}
