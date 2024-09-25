8c4fc32fee12d7b7c2dbd91bcda9e8bdcde116ee
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-09-25 16:11:26 +0000

    feat(leads): add lead store to module

--- ng-lead/src/app/features/leads/leads.module.ts
+++ ng-lead/src/app/features/leads/leads.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { LeadsRoutingModule } from './leads-routing.module';
+import { LeadsStoreModule } from './store/leads-store.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, LeadsRoutingModule],
+  imports: [CommonModule, LeadsRoutingModule, LeadsStoreModule],
 })
 export class LeadsModule {}
