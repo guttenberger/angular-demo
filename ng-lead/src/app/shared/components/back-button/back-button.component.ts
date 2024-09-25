34af782ecefc6cea7133ca7c0a1d55111108487e
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-09-25 14:42:22 +0000

    feat(core): add snackbar notification service

---
+++ ng-lead/src/app/shared/components/back-button/back-button.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule, Location } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+import { traverseAndGetCurrentRouteData$ } from '@shared/utils/route-utils';
+
+@Component({
+  selector: 'app-back-button',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatIconModule],
+  templateUrl: './back-button.component.html',
+  styleUrl: './back-button.component.scss',
+})
+export class BackButtonComponent {
+  // Services
+  readonly #location = inject(Location);
+  readonly #router = inject(Router);
+  readonly #route = inject(ActivatedRoute);
+
+  // State
+  readonly #currentRouteData$ = traverseAndGetCurrentRouteData$(
+    this.#router,
+    this.#route,
+  );
+  readonly showBackButtonSignal = toSignal(
+    this.#currentRouteData$.pipe(
+      map((data) => (data['hideBackButton'] ? false : true)),
+    ),
+  );
+
+  goBack() {
+    const isFirstPageInHistory = window.history.length === 0;
+
+    if (isFirstPageInHistory) {
+      this.#router.navigate(['/']);
+    } else {
+      this.#location.back();
+    }
+  }
+}
