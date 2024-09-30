1ffd0f3fb3b0e8ea1cc225edcc9f9a717b21849a
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-09-30 14:34:42 +0000

    fix(sidebar toggle): unsubscribe with takeUntilDestroyed

--- ng-lead/src/app/layout/components/sidebar/sidebar.component.ts
+++ ng-lead/src/app/layout/components/sidebar/sidebar.component.ts
@@ -6,7 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { Subscription } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { ResponsiveService } from '@core/services/responsive.service';
 import { SidebarService } from './services/sidebar.service';
@@ -22,27 +22,19 @@ export class SidebarComponent implements AfterViewInit {
   readonly #sidebarService = inject(SidebarService);
   readonly #responsiveService = inject(ResponsiveService);
 
-  // Subscriptions
-  readonly #subscriptions = new Subscription();
-
   // Template Bindings
   @ViewChild(MatSidenav) sideNav!: MatSidenav;
   @HostBinding('class.is-mobile') isMobile =
     this.#responsiveService.isMobileSignal();
 
   // State
+  protected toggleSidebar$ =
+    this.#sidebarService.toggleSidebar$.pipe(takeUntilDestroyed());
   protected isMobileSignal = this.#responsiveService.isMobileSignal;
 
   ngAfterViewInit(): void {
-    this.#toggleSidebar(); // Set up sidebar toggle event listener
-  }
-
-  #toggleSidebar(): void {
-    const sidebarToggleSubscription =
-      this.#sidebarService.toggleSidebar$.subscribe(() => {
-        this.sideNav.toggle();
-      });
-
-    this.#subscriptions.add(sidebarToggleSubscription);
+    this.toggleSidebar$.subscribe(() => {
+      this.sideNav.toggle();
+    });
   }
 }
