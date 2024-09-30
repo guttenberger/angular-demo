69f93d0e92b4a234ea3a044d605f29aef16c53fa
Alexander Guttenberger <contact.guttenberger@gmail.com>
2024-09-30 13:31:55 +0000

    style(leads module): remove unneeded module import

--- ng-lead/src/app/features/leads/leads.module.ts
+++ ng-lead/src/app/features/leads/leads.module.ts
@@ -1,11 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { LeadsRoutingModule } from './leads-routing.module';
 import { LeadsStoreModule } from './store/leads-store.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, LeadsRoutingModule, LeadsStoreModule],
+  imports: [LeadsRoutingModule, LeadsStoreModule],
 })
 export class LeadsModule {} // NgModule need to Lazy-load and register LeadsStoreModule once
